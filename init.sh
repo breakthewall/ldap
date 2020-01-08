9e8d86a03d3aea82b7b0f2c6f242b7b1442fce41
joan <joan.herisson@univ-evry.fr>
2020-01-08 15:55:12 +0100

    add pwm

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# OpenLDAP
 echo "LDAP_ADMIN_PASSWORD=`date +%s | shasum | base64 | head -c 32`" > .env/deploy.ldap
+
+# # GUI
+# git clone --branch v1_9 --single-branch https://github.com/pwm-project/pwm.git
+#
+# docker run -it --rm --name build-pwm \
+#   -v $PWD/pwm:/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean package
